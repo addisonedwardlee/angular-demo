a052fc89ecf9c10d995d0466db54f5db228a5a29
Addison <Addisonedwardlee@gmail.com>
2014-08-05 11:43:47 -0700

    moved tests

---
+++ test/unit/loginService_test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('loginService service', function() {
+  beforeEach(angular.mock.module('myApp'));
+
+  describe('login', function() {
+    it('should...', function() {
+
+    });
+  });
+});
---
+++ test/unit/login_test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('login module', function() {
+  beforeEach(angular.mock.module('login'));
+
+  describe('login', function() {
+    it('should...', function() {
+
+    });
+  });
+});
